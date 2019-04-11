253c00002689ca612ed4ddbf4795598dfddecc18
CS70 koala <xiyang@hmc.edu>
2019-04-11 14:01:24 -0700

    authentication

--- README.md
+++ README.md
@@ -7,4 +7,3 @@ usage: predict.py [-h] --title TITLE [--views VIEWS] [--likes LIKES]
                   [--dislikes DISLIKES] [--comments COMMENTS] [--imagepath IMAGEPATH]
                   
 Provide the script with the title of the video to analyze and, if known, the number of views, likes, dislikes, comments and thumbnail image path. The script will print the model prediction: 1 if the video is probably clickbait, 0 otherwise.
-
--- predict.py
+++ predict.py
@@ -1,5 +1,6 @@
 
 import argparse
+import os
 import re
 import emoji
 import numpy as np
@@ -7,7 +8,8 @@ import pandas as pd
 import pickle
 from gensim.parsing.preprocessing import *
 from fastai.vision import *
-
+import requests
+from io import BytesIO
 
 
 
@@ -72,97 +74,100 @@ def average_embedding(tokens, word2vec, na_vector=None):
 
     return np.mean(np.array(vectors), axis=0)
 
-
-parser = argparse.ArgumentParser(description="Predict if a Youtube video is clickbait or not.")
-parser.add_argument(
-    "--title", "-t",
-    type=str, help="Title.", required=True)
-parser.add_argument(
-    "--views", "-v",
-    type=int, help="Number of views.", required=False)
-parser.add_argument(
-    "--likes", "-l",
-    type=int, help="Number of likes.", required=False)
-parser.add_argument(
-    "--dislikes", "-d",
-    type=int, help="Number of dislikes.", required=False)
-parser.add_argument(
-    "--comments", "-c",
-    type=int, help="Number of comments.", required=False)
-parser.add_argument(
-    "--imagepath", "-i",
-    type=str, help="Thumbnail image path.", required=False)
-args = parser.parse_args()
-
-
-# Import the Word2Vec model and the mean vector representation computed on the train set:
-word2vec = pickle.load(open("word2vec", "rb"))
-mean_title_embedding = pickle.load(open("mean-title-embedding", "rb"))
+def main():
+    print
+    parser = argparse.ArgumentParser(description="Predict if a Youtube video is clickbait or not.")
+    parser.add_argument(
+        "--title", "-t",
+        type=str, help="Title.", required=True)
+    parser.add_argument(
+        "--views", "-v",
+        type=int, help="Number of views.", required=False)
+    parser.add_argument(
+        "--likes", "-l",
+        type=int, help="Number of likes.", required=False)
+    parser.add_argument(
+        "--dislikes", "-d",
+        type=int, help="Number of dislikes.", required=False)
+    parser.add_argument(
+        "--comments", "-c",
+        type=int, help="Number of comments.", required=False)
+    parser.add_argument(
+        "--imageurl", "-i",
+        type=str, help="Thumbnail image url.", required=False)
+    args = parser.parse_args()
 
 
-input = {
-    "video_title": args.title,
-    "video_views": args.views if args.views is not None else np.NaN,
-    "video_likes": args.likes if args.likes is not None else np.NaN,
-    "video_dislikes": args.dislikes if args.dislikes is not None else np.NaN,
-    "video_comments": args.comments if args.comments is not None else np.NaN,
-}
-sample = pd.DataFrame([ input ])
+    # Import the Word2Vec model and the mean vector representation computed on the train set:
+    word2vec = pickle.load(open("word2vec", "rb"))
+    mean_title_embedding = pickle.load(open("mean-title-embedding", "rb"))
 
-# Tokenize the title and then compute its embedding:
-sample["video_title"] = sample["video_title"].apply(tokenize)
-sample["video_title"] = sample["video_title"].apply(
-    average_embedding, word2vec=word2vec, na_vector=mean_title_embedding)
-sample = pd.concat(
-    [
-        sample[["video_views", "video_likes", "video_dislikes", "video_comments"]],
-        sample["video_title"].apply(pd.Series)
-    ], axis=1)
 
-# Compute the log of the video metadata or replace the missing values with the mean values obtained
-# from the train set:
-mean_log_video_views = pickle.load(open("mean-log-video-views", "rb"))
-mean_log_video_likes = pickle.load(open("mean-log-video-likes", "rb"))
-mean_log_video_dislikes = pickle.load(open("mean-log-video-dislikes", "rb"))
-mean_log_video_comments = pickle.load(open("mean-log-video-comments", "rb"))
+    input = {
+        "video_title": args.title,
+        "video_views": args.views if args.views is not None else np.NaN,
+        "video_likes": args.likes if args.likes is not None else np.NaN,
+        "video_dislikes": args.dislikes if args.dislikes is not None else np.NaN,
+        "video_comments": args.comments if args.comments is not None else np.NaN,
+    }
+    sample = pd.DataFrame([ input ])
 
-sample[["video_views", "video_likes", "video_dislikes", "video_comments"]] = \
-    sample[["video_views", "video_likes", "video_dislikes", "video_comments"]].apply(np.log)
+    # Tokenize the title and then compute its embedding:
+    sample["video_title"] = sample["video_title"].apply(tokenize)
+    sample["video_title"] = sample["video_title"].apply(
+        average_embedding, word2vec=word2vec, na_vector=mean_title_embedding)
+    sample = pd.concat(
+        [
+            sample[["video_views", "video_likes", "video_dislikes", "video_comments"]],
+            sample["video_title"].apply(pd.Series)
+        ], axis=1)
 
-if sample["video_views"].isnull().any():
-    sample["video_views"].fillna(mean_log_video_views, inplace=True)
-if sample["video_likes"].isnull().any():
-    sample["video_likes"].fillna(mean_log_video_likes, inplace=True)
-if sample["video_dislikes"].isnull().any():
-    sample["video_dislikes"].fillna(mean_log_video_dislikes, inplace=True)
-if sample["video_comments"].isnull().any():
-    sample["video_comments"].fillna(mean_log_video_comments, inplace=True)
+    # Compute the log of the video metadata or replace the missing values with the mean values obtained
+    # from the train set:
+    mean_log_video_views = pickle.load(open("mean-log-video-views", "rb"))
+    mean_log_video_likes = pickle.load(open("mean-log-video-likes", "rb"))
+    mean_log_video_dislikes = pickle.load(open("mean-log-video-dislikes", "rb"))
+    mean_log_video_comments = pickle.load(open("mean-log-video-comments", "rb"))
 
-# Replace any -Inf value with 0:
-sample = sample.replace(-np.inf, 0)
+    sample[["video_views", "video_likes", "video_dislikes", "video_comments"]] = \
+        sample[["video_views", "video_likes", "video_dislikes", "video_comments"]].apply(np.log)
 
-# Import the min-max scaler and apply it to the sample:
-min_max_scaler = pickle.load(open("min-max-scaler", "rb"))
-sample = pd.DataFrame(min_max_scaler.transform(sample), columns=sample.columns)
+    if sample["video_views"].isnull().any():
+        sample["video_views"].fillna(mean_log_video_views, inplace=True)
+    if sample["video_likes"].isnull().any():
+        sample["video_likes"].fillna(mean_log_video_likes, inplace=True)
+    if sample["video_dislikes"].isnull().any():
+        sample["video_dislikes"].fillna(mean_log_video_dislikes, inplace=True)
+    if sample["video_comments"].isnull().any():
+        sample["video_comments"].fillna(mean_log_video_comments, inplace=True)
 
-# Import the SVM model:
-svm = pickle.load(open("svm", "rb"))
+    # Replace any -Inf value with 0:
+    sample = sample.replace(-np.inf, 0)
 
-# Load image model
-classes = ['clickbait','non_clickbait']
-path = '/Users/CarrieYang/Desktop/parse_csv'
-data = ImageDataBunch.single_from_classes(path, classes, ds_tfms=get_transforms(), size=224).normalize(imagenet_stats)
-learn = create_cnn(data, models.resnet50)
-learn.load('clickbait-model')
-image_path = args.imagepath
+    # Import the min-max scaler and apply it to the sample:
+    min_max_scaler = pickle.load(open("min-max-scaler", "rb"))
+    sample = pd.DataFrame(min_max_scaler.transform(sample), columns=sample.columns)
 
-im = open_image(image_path)
-# Print image prediction:
-img_pred = learn.predict(im)[2][0]
+    # Import the SVM model:
+    svm = pickle.load(open("svm", "rb"))
 
-# Print title prediction:
-title_pred = svm.predict_proba(sample)[0][1]
+    # Load image model
+    classes = ['clickbait','non_clickbait']
+    path = os.getcwd()                
+    data = ImageDataBunch.single_from_classes(path, classes, ds_tfms=get_transforms(), size=224).normalize(imagenet_stats)
+    learn = cnn_learner(data, models.resnet50)
+    learn.load('clickbait-model')
+    img_data = BytesIO(requests.get(args.imageurl).content)
+    im = open_image(img_data)
+    # Print image prediction:
+    img_pred = learn.predict(im)[2][0]
 
-# Print clickbait probability
+    # Print title prediction:
+    title_pred = svm.predict_proba(sample)[0][1]
 
-print((img_pred+title_pred)/2)
+    # Print clickbait probability
+
+    return (img_pred+title_pred)/2
+
+
+print(main())
